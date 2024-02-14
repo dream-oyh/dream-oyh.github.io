641506e4fd891364159e13690ad58c01039105a8
dream同学0 <1399541701@qq.com>
2024-02-15 00:51:25 +0800

    deploy:20240215-00:51:25

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 git init
 git add -A
 git commit -m 'deploy:'$(date "+%Y%m%d-%H:%M:%S")
-git checkout-b code_hope
+git checkout -b code_hope
 git push git@github.com:dream-oyh/dream-oyh.github.io.git code_hope
 
 pnpm build
