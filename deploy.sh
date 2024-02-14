73b3cdab2b399437db0f9c4a8133e515024200e4
dream同学0 <1399541701@qq.com>
2024-02-15 00:51:07 +0800

    deploy:20240215-00:51:07

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 git init
 git add -A
 git commit -m 'deploy:'$(date "+%Y%m%d-%H:%M:%S")
-git branch -b code_hope
+git checkout-b code_hope
 git push git@github.com:dream-oyh/dream-oyh.github.io.git code_hope
 
 pnpm build
@@ -9,7 +9,7 @@ cd src/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy:'$(date "+%Y%m%d-%H:%M:%S")
-git branch -b main_hope
+git checkout -b main_hope
 git push git@github.com:dream-oyh/dream-oyh.github.io.git -f main_hope
 exec /bin/bash
 cd -
