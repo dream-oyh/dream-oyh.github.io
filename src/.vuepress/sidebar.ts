1e3dfe6cb55fcb9da02d29c8de53a88e911ff0c0
dream同学0 <1399541701@qq.com>
2024-02-29 15:22:52 +0800

    deploy:20240229-15:22:52

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -169,6 +169,7 @@ export default sidebar({
                     "software.md",
                     "web.md",
                     "film_recommend.md",
+                    "CSU.md",
                 ]
             },
             {
