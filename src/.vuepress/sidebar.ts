3ff359ed59cae6c2a8066c2e73dfbc616c530acd
lxl66566 <lxl66566@gmail.com>
2024-04-11 14:26:10 +0800

    fix article page unexpected behavior

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,218 +1,213 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
-  "/code/": [
-    {
-      text: "编程",
-      link: "/code/",
-      children: [
-        "html.md",
-        "markdown.md",
-        "python.md",
-        {
-          text: "Arduino 开发板",
-          icon: "arduino",
-          link: "/code/Arduino/",
-        },
-        "cpp.md",
-      ],
-    },
-    {
-      text: "工具",
-      children: [
-        "git.md",
-        "vscode.md",
-        "latex.md",
-        "shell.md",
-        "vim.md",
-        "Linux.md",
-        "github.md",
-      ],
-    },
-    {
-      text: "Python 库",
-      prefix: "python/",
-      collapsible: true,
-      children: [
-        "pandas.md",
-        {
-          text: "Pytorch",
-          icon: "pytorch",
-          link: "pytorch/1pytorch.md",
-        },
-        "Seaborn.md",
-        "web_crawler.md",
-        "websocket.md",
-        "LocalAugment.md",
-      ],
-    },
-    {
-      text: "锦囊",
-      collapsible: true,
-      children: ["tips/regex.md", "tips/pdf2docx.md"],
-    },
-  ],
-  "/code/python/pytorch/": [
-    {
-      text: "Pytorch 学习笔记",
-      icon: "pytorch",
-      children: [
-        {
-          text: "Pytorch 基础",
-          link: "1pytorch.md",
-        },
-        {
-          text: "常见的模型源码实现",
-          collapsible: true,
-          children: [
-            "2.1linear_regression.md",
-            "2.2FashionMNIST.md",
-            "2.3softmax.md",
-            "2.4MLP.md",
-          ],
-        },
-        {
-          text: "走进深度学习",
-          link: "3.1Deeplearning_basic.md",
-        },
-        {
-          text: "卷积神经网络",
-          collapsible: true,
-          children: [
-            "4.1convolutional_nn_basic.md",
-            "4.2LeNet.md",
-            "4.3AlexNet.md",
-            "4.4VGG.md",
-            "4.5NiN.md",
-            "4.6GoogleNet.md",
-          ],
-        },
-        "img.md",
-      ],
-    },
-  ],
-  "/english/": [
-    {
-      text: "主题语料库",
-      collapsible: true,
-      icon: "diary",
-      prefix: "repo/",
-      link: "repo/index.md",
-      children: [
-        "negative.md",
-        "life.md",
-        "growth.md",
-        "community.md"
-      ]
-    },
-    {
-      text: "英语视频词汇积累",
-      collapsible: true,
-      icon: "state",
-      prefix: "video/",
-      children: [
-        "1.md",
-        "2.md",
-        "3.md",
-        "4.md",
-        "5.md",
-        "6.md",
-        "7.md",
-        "8.md",
-        "9.md",
-      ],
-    },
-    {
-      text: "FREE 大学英语阅读写作能力提升课",
-      collapsible: true,
-      icon: "build",
-      prefix: "free/",
-      children: [
-        "1.md",
-        "2.md",
-        "3.md",
-        "4.md",
-        "5.md",
-        "6.md",
-        "7.md",
-        "8.md",
-        "9.md",
-      ],
-    },
-    {
-      text: "学习强国官方英语资源",
-      collapsible: true,
-      icon: "Article",
-      prefix: "china/",
-      children: [
-        "10.22.md",
-        "10.23.md",
-        "10.24.md",
-        "10.25.md",
-        "10.26.md",
-        "10.27.md",
-        "10.28.md",
-        "10.29.md",
-        "10.30.md",
-        "10.31.md",
-        "11.1.md",
-        "11.2.md",
-      ],
-    },
-  ],
-  "/article/": [
-    {
-      text: "随笔",
-      icon: "pen",
-      children: ["article.md", "weakness.md"],
-    },
-    {
-      text: "推荐",
-      icon: "34wujiaoxingpingfenshixin",
-      children: ["software.md", "web.md", "film_recommend.md"],
-    },
-    {
-      text: "学习路线",
-      icon: "graduate",
-      children: ["CSU.md", "baoyan.md"],
-    },
-    {
-      text: "读书笔记",
-      icon: "note",
-      children: ["read.md", "hamlet.md"],
-    },
-    {
-      text: "他山之石",
-      icon: "communityfill",
-      link: "external.md",
-    },
-  ],
-  "/credit/": [
-    {
-      text: "学习资料",
-      link: "/credit/",
-    },
-  ],
-  "/blog/": [
-    {
-      text: "关于博客",
-      link: "/blog/",
-    },
-    {
-      text: "博客日志",
-      link: "log.md",
-    },
-    {
-      text: "问题列表",
-      link: "vuepress.md",
-    },
-    {
-      text: "博客日程",
-      link: "schedule.md",
-    },
-    {
-      text: "博客常用站点跳转",
-      link: "spots.md",
-    },
-  ],
+	"/code/": [
+		{
+			text: "编程",
+			link: "/code/",
+			children: [
+				"html.md",
+				"markdown.md",
+				"python.md",
+				{
+					text: "Arduino 开发板",
+					icon: "arduino",
+					link: "/code/Arduino/",
+				},
+				"cpp.md",
+			],
+		},
+		{
+			text: "工具",
+			children: [
+				"git.md",
+				"vscode.md",
+				"latex.md",
+				"shell.md",
+				"vim.md",
+				"Linux.md",
+				"github.md",
+			],
+		},
+		{
+			text: "Python 库",
+			prefix: "python/",
+			collapsible: true,
+			children: [
+				"pandas.md",
+				{
+					text: "Pytorch",
+					icon: "pytorch",
+					link: "pytorch/1pytorch.md",
+				},
+				"Seaborn.md",
+				"web_crawler.md",
+				"websocket.md",
+				"LocalAugment.md",
+			],
+		},
+		{
+			text: "锦囊",
+			collapsible: true,
+			children: ["tips/regex.md", "tips/pdf2docx.md"],
+		},
+	],
+	"/code/python/pytorch/": [
+		{
+			text: "Pytorch 学习笔记",
+			icon: "pytorch",
+			children: [
+				{
+					text: "Pytorch 基础",
+					link: "1pytorch.md",
+				},
+				{
+					text: "常见的模型源码实现",
+					collapsible: true,
+					children: [
+						"2.1linear_regression.md",
+						"2.2FashionMNIST.md",
+						"2.3softmax.md",
+						"2.4MLP.md",
+					],
+				},
+				{
+					text: "走进深度学习",
+					link: "3.1Deeplearning_basic.md",
+				},
+				{
+					text: "卷积神经网络",
+					collapsible: true,
+					children: [
+						"4.1convolutional_nn_basic.md",
+						"4.2LeNet.md",
+						"4.3AlexNet.md",
+						"4.4VGG.md",
+						"4.5NiN.md",
+						"4.6GoogleNet.md",
+					],
+				},
+				"img.md",
+			],
+		},
+	],
+	"/english/": [
+		{
+			text: "主题语料库",
+			collapsible: true,
+			icon: "diary",
+			prefix: "repo/",
+			link: "repo/index.md",
+			children: ["negative.md", "life.md", "growth.md", "community.md"],
+		},
+		{
+			text: "英语视频词汇积累",
+			collapsible: true,
+			icon: "state",
+			prefix: "video/",
+			children: [
+				"1.md",
+				"2.md",
+				"3.md",
+				"4.md",
+				"5.md",
+				"6.md",
+				"7.md",
+				"8.md",
+				"9.md",
+			],
+		},
+		{
+			text: "FREE 大学英语阅读写作能力提升课",
+			collapsible: true,
+			icon: "build",
+			prefix: "free/",
+			children: [
+				"1.md",
+				"2.md",
+				"3.md",
+				"4.md",
+				"5.md",
+				"6.md",
+				"7.md",
+				"8.md",
+				"9.md",
+			],
+		},
+		{
+			text: "学习强国官方英语资源",
+			collapsible: true,
+			icon: "Article",
+			prefix: "china/",
+			children: [
+				"10.22.md",
+				"10.23.md",
+				"10.24.md",
+				"10.25.md",
+				"10.26.md",
+				"10.27.md",
+				"10.28.md",
+				"10.29.md",
+				"10.30.md",
+				"10.31.md",
+				"11.1.md",
+				"11.2.md",
+			],
+		},
+	],
+	"/articles/": [
+		{
+			text: "随笔",
+			icon: "pen",
+			children: ["article.md", "weakness.md"],
+		},
+		{
+			text: "推荐",
+			icon: "34wujiaoxingpingfenshixin",
+			children: ["software.md", "web.md", "film_recommend.md"],
+		},
+		{
+			text: "学习路线",
+			icon: "graduate",
+			children: ["CSU.md", "baoyan.md"],
+		},
+		{
+			text: "读书笔记",
+			icon: "note",
+			children: ["read.md", "hamlet.md"],
+		},
+		{
+			text: "他山之石",
+			icon: "communityfill",
+			link: "external.md",
+		},
+	],
+	"/credit/": [
+		{
+			text: "学习资料",
+			link: "/credit/",
+		},
+	],
+	"/blog/": [
+		{
+			text: "关于博客",
+			link: "/blog/",
+		},
+		{
+			text: "博客日志",
+			link: "log.md",
+		},
+		{
+			text: "问题列表",
+			link: "vuepress.md",
+		},
+		{
+			text: "博客日程",
+			link: "schedule.md",
+		},
+		{
+			text: "博客常用站点跳转",
+			link: "spots.md",
+		},
+	],
 });
---
+++ src/articles/web.md
@@ -0,0 +1,91 @@
+# 网站推荐
+
+## 数模论文绘图网站推荐
+
+### 色彩空间 ColorSpace 
+
+网址（单击即可跳转）：[ColorSpace](https://mycolor.space/)
+
+输入一种颜色后，可生成许多种调色搭配，相较于下面推荐的Adobe Color更具可选性，方案选择更多样
+
+### 素材网站 Freepik
+
+网址（单击即可跳转）：[Freepik](https://www.freepik.com/)
+
+外网，用于查找各种高清制图素材，需要用英文检索
+
+### Python Seaborn库绘图教程
+
+网址（单击即可跳转）：[知乎-Seaborn库绘图教程](https://zhuanlan.zhihu.com/p/81553421?utm_source=wechat_session)
+
+知乎老哥一篇详细的教授有关Seaborn库绘图教程，学！
+
+### MATLAB绘图
+
+网址（单击即可跳转）：[知乎-阿昆的科研日常](https://www.zhihu.com/column/c_1074615528869531648)
+
+该博主专栏中有极其丰富的MATLAB绘图教程，可查阅学习
+
+### BioLadder 绘图网站
+
+网址（单击即可跳转）：[BioLadder](https://www.bioladder.cn/web/#/pro/index)
+
+### 西电MSC论文插图制作博客
+
+网址（单击即可跳转）：[西电MSC博客](https://levitate-qian.github.io/2020/05/04/10%E7%B1%BB%E6%A1%88%E4%BE%8B%E5%B8%A6%E4%BD%A0%E4%BA%86%E8%A7%A3%E8%AE%BA%E6%96%87%E6%8F%92%E5%9B%BE%E5%88%B6%E4%BD%9C/)
+
+## 绝对值_x的个人主页
+
+网址（单击即可跳转）：[绝对值_x的个人主页](https://lxl66566.github.io/)
+
+本博客最初即是模仿该网站制作而成，网站中内容极其丰富，对于编程党极度友好！还有很多很多宝藏软件的推荐！
+
+## SPSSPRO
+
+网址（单击即可跳转）：[SPSSPRO](https://www.spsspro.com/)
+
+**作用：** 免费的分析数据网页，提供多种数据分析模型，这里的“多种”真的真的很多很多
+
+机器学习，线性规划，非线性规划……等等，一堆数学建模需要用到的模型都可以在这里找到！！甚至还提供了画图功能，matlab是什么我不知道哈哈哈哈哈
+
+目前没发现缺点，若发现缺点会在此处更新
+
+## Ubuntu Pastebin 代码高亮
+
+网址（单击即可跳转）：[Ubuntu Pastebin](https://pastebin.ubuntu.com/)
+
+缺点：需要注册一个账号；复制出来的代码会带有灰色底色，不知如何去除
+
+作用：matlab代码，c++代码直接从编译器粘贴到word上是没有关键字高亮显示的，但是通过这个网站转化得到的代码，粘贴到word中可以显示高亮
+
+数学建模论文附录代码又可以用上了嘿嘿嘿，感谢清风老师的推荐捏~
+
+## 高德地图API
+
+网址（单击即可跳转）：[高德地图API](https://lbs.amap.com/tools/picker)
+
+作用：通过高德地图查询任一地点的经纬度
+
+对于测绘的同学可能会有用哦~
+
+## Adobe color
+
+网址（单击即可跳转）：[Adobe color](https://color.adobe.com/zh/create/color-wheel)
+
+Adobe 官方推出的一款选色器，输入C位颜色色号，自动匹配与之对应的渐变色，可用于PPT，PS等各类用于图形设计软件的渐变色色彩选取中。
+
+## officeplus——office官方模板平台
+
+网址（单击即可跳转）：[officeplus](https://www.officeplus.cn/)
+
+**免费**的PPT，Word，Excel模板网站，官方推出，别再上百度一顿乱找模板了，官方给的不要白不要？
+
+而且能搜到各高校的PPT制作模板（好的中南大学没有），比学校自己给的好看多啦
+
+## 在线Latex公式编辑器
+
+网址（单击即可跳转）：[在线Latex公式编辑器](https://www.latexlive.com/)
+
+up主[妈咪说](https://www.bilibili.com/video/BV14g4y1q7pb?spm_id_from=333.999.0.0)开发的网站
+
+用可视化界面输入数学公式，网站会自动帮你转成latex语法，学习latex新手小白必备网站
