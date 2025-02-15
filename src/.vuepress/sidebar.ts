6b01f41165105b840889e81f0b8a7ac2d940611c
dream-oyh <1399541701@qq.com>
2025-02-15 19:30:36 +0800

    deploy:20250215-19:30:36

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -19,7 +19,7 @@ export default sidebar({
     },
     {
       text: "工具",
-      children: ["git.md", "latex.md", "shell.md", "vim.md", "Linux.md", "github.md","vscode.md"],
+      children: ["git.md", "latex.md", "shell.md", "vim.md", "Linux.md", "docker.md", "github.md", "vscode.md"],
     },
     {
       text: "Python 库",
